1b1bf1049b17d82eb9986a049919af56be4a67fa
SIDDHARTH SUTARIYA <dreamit0708@gmail.com>
2024-07-12 07:53:18 +0530

    commit final

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,6 +1,7 @@
 // src/Service/GlobalApi.jsx
-// const Base_Url = "http://192.168.86.11:3000/";
-const Base_Url = import.meta.env.SUB_BASE_URL || "http://192.168.86.11:3000/";
+// console.log("VITE_SUB_BASE_URL:", import.meta.env.SUB_BASE_URL);
+const Base_Url = "http://192.168.50.11:3001/";
+// const Base_Url = import.meta.env.SUB_BASE_URL || "http://192.168.86.11:3000/";
 
 export const apiConfig = {
     Base_Url
