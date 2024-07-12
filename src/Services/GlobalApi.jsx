b84be2d4e7873fdfbe63fe9605e7845eb3facf13
SIDDHARTH SUTARIYA <dreamit0708@gmail.com>
2024-07-12 14:22:42 +0530

    commit render on

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,6 +1,6 @@
 // src/Service/GlobalApi.jsx
 // console.log("VITE_SUB_BASE_URL:", import.meta.env.SUB_BASE_URL);
-const Base_Url = "http://192.168.50.11:3001/";
+const Base_Url = "https://vkms-server.onrender.com/";
 // const Base_Url = import.meta.env.SUB_BASE_URL || "http://192.168.86.11:3000/";
 
 export const apiConfig = {
