074df267718d82a927e6d93c651a48562f0fd7dd
SIDDHARTH SUTARIYA <dreamit0708@gmail.com>
2024-07-12 07:43:37 +0530

    commit

--- roombooking-system/models/gallery.js
+++
@@ -1,8 +0,0 @@
-// models/gallery.js
-class Gallery {
-    constructor(image_url) {
-        this.image_url = image_url;
-    }
-}
-
-module.exports = Gallery;
--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,5 +1,6 @@
 // src/Service/GlobalApi.jsx
-const Base_Url = "http://192.168.86.11:3000/";
+// const Base_Url = "http://192.168.86.11:3000/";
+const Base_Url = import.meta.env.SUB_BASE_URL || "http://192.168.86.11:3000/";
 
 export const apiConfig = {
     Base_Url
